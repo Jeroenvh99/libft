efb575c65f041e71d7186f2eb4cd4031c45b3342
Jeroen Van Halderen <jvan-hal@f0r2s12.codam.nl>
2024-01-11 20:16:32 +0100

    improved the function

--- ft_atof.c
+++ ft_atof.c
@@ -6,30 +6,43 @@
 /*   By: jvan-hal <jvan-hal@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/01/10 12:05:39 by jvan-hal      #+#    #+#                 */
-/*   Updated: 2024/01/10 14:33:50 by jvan-hal      ########   odam.nl         */
+/*   Updated: 2024/01/11 20:07:43 by jvan-hal      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	checksign(char **str)
+{
+	int	sign;
+
+	sign = 1;
+	if (**str == '+' || **str == '-')
+	{
+		if (**str == '-')
+			sign = -1;
+		++(*str);
+	}
+	return (sign);
+}
+
 double	ft_atof(char *str)
 {
 	double	val;
+	int		sign;
 	int		significant;
 
 	val = 0;
+	sign = checksign(&str);
 	significant = 0;
-	while (*str)
+	while (*str && (ft_isdigit(*str) || *str == '.'))
 	{
-		if (!ft_isdigit(*str) && *str != '.' && val > 0)
-			break ;
 		if (*str != '.')
 		{
 			val = (val * 10) + (*str - '0');
-			if (significant > 0)
-				significant *= 10;
+			significant *= 10;
 		}
-		else
+		else if (*str == '.')
 		{
 			if (significant > 0)
 				return (0);
@@ -37,6 +50,7 @@ double	ft_atof(char *str)
 		}
 		++str;
 	}
-	val /= significant;
-	return (val);
+	if (val > 0 && significant > 0)
+		val /= significant;
+	return (val * sign);
 }
